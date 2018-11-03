505fab8ab4e58f19250e76716efb54b3884bbc7d
Eddi <max@web.de>
2018-11-03 18:32:09 +0100

    add Liste and ListElements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Liste from './Liste'
 
 class App extends Component {
   
@@ -7,7 +8,8 @@ class App extends Component {
   render() {
     return (
     	<div className="App">
-    		<h2>Hier wird gearbeitet.</h2>	
+    		<h2>Hier wird gearbeitet.</h2>
+    		<Liste></Liste>
     	</div>
     );
   }
